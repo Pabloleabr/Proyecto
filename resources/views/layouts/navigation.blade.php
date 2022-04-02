713df08107768ef58f755dae3eacb2afcb10b9d5
Pablo Leal del Ojo <pabolo333@gmail.com>
2022-04-02 19:05:47 +0200

    nav-menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,93 +2,104 @@
     <!-- Primary Navigation Menu -->
     <div class="">
         <div class="">
-            <div class="">
+            <!-- header -->
+            <div class="flex pt-2 w-max barra pl-2 pb-1 justify-between">
                 <!-- Logo -->
-                <div class="">
-                    <a href="{{ route('ver-ejercicios') }}">
-
+                <div class="flex">
+                    <a class="w-10" href="{{ route('ver-ejercicios') }}">
+                        <img width="100%" src="{{URL::to('/')}}/img/logo.png" alt="logo del sitio web">
                     </a>
+                    <h2 class="p-2">
+                        PreguntasPro
+                    </h2>
                 </div>
+                <!--login-->
+                <div class="flex p-2 ">
+                    @if (empty(Auth::user()))
+                    @if (Route::has('login'))
+                    <div class="">
+                        @auth
+                            <a href="{{ url('/dashboard') }}" class="pl-2">Dashboard</a>
+                        @else
+                            <a href="{{ route('login') }}" class="pl-2">Log in</a>
 
-                <div class="">
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="pl-2">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                    @endif
+                @else
+                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="flex transition duration-150 ease-in-ouflex items-center ">
+                                    <div>{{ Auth::user()->name }}</div>
+
+                                    <div class="ml-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <x-dropdown-link :href="route('logout')"
+                                            onclick="event.preventDefault();
+                                                        this.closest('form').submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+
+                    <!-- Hamburger -->
+                    <div class="-mr-2 flex items-center sm:hidden">
+                        <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                                <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+
+                @endif
+            </div>
+
+
+            </div>
+            <!--navbar-->
+            <div id="nav">
+
                     <x-nav-link :href="route('ver-ejercicios')" :active="request()->routeIs('ver-ejercicios')">
                         Ejercicios
                     </x-nav-link>
-                </div>
+
 
                 @if (!empty(Auth::user()))
                 <!--Navigation Links for users-->
-                <div class="">
+
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Mis Ejercicios') }}
                     </x-nav-link>
-                </div>
 
 
-                <div class="">
+
+
                     <x-nav-link :href="route('crear-ejer')" :active="request()->routeIs('crear-ejer')">
                         Crear Ejercicio
                     </x-nav-link>
-                </div>
+
 
                 @endif
-
             </div>
-            @if (empty(Auth::user()))
-                @if (Route::has('login'))
-                <div class="">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="">Register</a>
-                        @endif
-                    @endauth
-                </div>
-                @endif
-            @else
-                <div class="hidden sm:flex sm:items-center sm:ml-6">
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                <div>{{ Auth::user()->name }}</div>
-
-                                <div class="ml-1">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                            </button>
-                        </x-slot>
-
-                        <x-slot name="content">
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-
-                                <x-dropdown-link :href="route('logout')"
-                                        onclick="event.preventDefault();
-                                                    this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-dropdown>
-                </div>
-
-                <!-- Hamburger -->
-                <div class="-mr-2 flex items-center sm:hidden">
-                    <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-
-            @endif
             <!-- Settings Dropdown -->
         </div>
     </div>
